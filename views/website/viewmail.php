9753851e131fd7b29d31732b37eaa14f59bb5f6c
Nathan Yassin <codeventior@gmail.com>
2023-10-18 13:39:49 +0200

    Unique id voor emails

---
+++ views/website/viewmail.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: rgb(77, 98, 98);
+        }
+
+        .content {
+            display: flex;
+            justify-content: center;
+        }
+
+        .box {
+            width: 500px;
+            height: 700px;
+
+            background-color: rgb(124, 127, 135);
+        }
+
+        th {
+            border: 3px solid black;
+        }
+
+        td {
+            background-color: darkgrey;
+            height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="content">
+        <div class="box">
+            
+        </div>
+    </div>
+</body>
+
+</html>
